7309d3b2067d850beac424de85d810605a472be5
ms-nenova <ms.nenova@gmail.com>
2014-11-09 01:13:20 +0200

    Identity models extracted

---
+++ PhotoSchool.Data/PhotoSchoolDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace PhotoSchool.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using PhotoSchool.Models;
+
+    public class PhotoSchoolDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public PhotoSchoolDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static PhotoSchoolDbContext Create()
+        {
+            return new PhotoSchoolDbContext();
+        }
+    }
+}
--- PhotoSchool.Models/ApplicationUser.cs
+++ PhotoSchool.Models/ApplicationUser.cs
@@ -1,11 +1,10 @@
-﻿using System.Data.Entity;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-
-namespace PhotoSchool.Models
+﻿namespace PhotoSchool.Models
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
@@ -15,19 +14,6 @@ namespace PhotoSchool.Models
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
             return userIdentity;
-        }
-    }
-
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
         }
     }
 }
