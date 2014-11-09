f334c60c2bd992dc73a66c65a92a3cdec8d0857c
ms-nenova <ms.nenova@gmail.com>
2014-11-09 20:39:23 +0200

    Repository pattern

---
+++ PhotoSchool.Data.Contracts/Repository/IDeletableEntityRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace PhotoSchool.Data.Contracts.Repository
+{
+    using System.Linq;
+
+    public interface IDeletableEntityRepository<T> : IRepository<T> where T : class
+    {
+        IQueryable<T> AllWithDeleted();
+    }
+}
