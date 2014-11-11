0acff602985cd47f8c2c91d17037ae5c9b776675
ms-nenova <ms.nenova@gmail.com>
2014-11-11 15:36:54 +0200

    Razor and antiforgery configured

--- PhotoSchool/App_Start/BundleConfig.cs
+++ PhotoSchool/App_Start/BundleConfig.cs
@@ -8,11 +8,24 @@ namespace PhotoSchool
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery")
+                //.Include("~/Scripts/jquery-{version}.js"));
+            .Include("~/Scripts/kendo/jquery.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryajax")
+            //.Include("~/Scripts/jquery-{version}.js",
+            // "~/Scripts/jquery.unobtrusive-ajax.js"));
+            .Include("~/Scripts/jquery.unobtrusive-ajax.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/kendo")
+            .Include("~/Scripts/kendo/kendo.all.min.js",
+            "~/Scripts/kendo/kendo.aspnetmvc.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+                        "~/Scripts/custom.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -26,6 +39,10 @@ namespace PhotoSchool
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/kendo").Include("~/Content/kendo/kendo.common.min.css",
+                        "~/Content/kendo/kendo.common-bootstrap.min.css",
+                        "~/Content/kendo/kendo.silver.min.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
--- PhotoSchool/Global.asax.cs
+++ PhotoSchool/Global.asax.cs
@@ -1,9 +1,11 @@
-﻿using PhotoSchool.Web.Infrastructure.Mapping;
+﻿using PhotoSchool.App_Start;
+using PhotoSchool.Web.Infrastructure.Mapping;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -14,6 +16,8 @@ namespace PhotoSchool
     {
         protected void Application_Start()
         {
+            ViewEnginesConfig.RegisterViewEngines(ViewEngines.Engines);
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
@@ -21,6 +25,8 @@ namespace PhotoSchool
 
             var autoMapperConfig = new AutoMapperConfig(Assembly.GetExecutingAssembly());
             autoMapperConfig.Execute();
+
+            AntiForgeryConfig.SuppressXFrameOptionsHeader = true;
         }
     }
 }
